6558d6fb0f0b830f375ab8618e826f35efe54cfd
Genoss21 <135839833+Genoss21@users.noreply.github.com>
2025-02-03 13:45:37 +0800

    Add files via upload

---
+++ attendance_registration_system/src/test/java/dev/tgsi/attendance_registration_system/AttendanceRegistrationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.tgsi.attendance_registration_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendanceRegistrationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
