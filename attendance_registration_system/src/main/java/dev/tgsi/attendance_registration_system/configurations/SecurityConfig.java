263898065996f98d318e1acea7a14d7a92eba299
Tsukijay08 <delacruz.jojitjr.d.1458@gmail.com>
2025-02-17 08:27:40 +0800

    Revising Login form (textbox border will turn red when no credentials

--- attendance_registration_system/src/main/java/dev/tgsi/attendance_registration_system/configurations/SecurityConfig.java
+++ attendance_registration_system/src/main/java/dev/tgsi/attendance_registration_system/configurations/SecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
@@ -29,17 +30,17 @@ public class SecurityConfig {
         this.customSuccessHandler = customSuccessHandler;
     }
 
-    //@SuppressWarnings("deprecation")
+    @SuppressWarnings("deprecation")
     @Bean
-    //public static NoOpPasswordEncoder passwordEncoder() {
-    //    return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
-    //}
-    public AuthenticationProvider authenticationProvider(){
+    public static NoOpPasswordEncoder passwordEncoder() {
+        return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
+    }
+   /*  public AuthenticationProvider authenticationProvider(){
         DaoAuthenticationProvider provider =  new DaoAuthenticationProvider();
         provider.setUserDetailsService(userDetailsService);
         provider.setPasswordEncoder(new BCryptPasswordEncoder());
         return provider;
-    }
+    }*/
     
 
     @Bean
