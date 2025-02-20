d3c800780c58a7f07a68536587f3aa76ad9010db
jude <quimiojude@gmail.com>
2025-02-20 08:12:47 +0800

    addding jsonignoreproperties annotation to avoid excessive nesting in json response

--- attendance_registration_system/src/main/java/dev/tgsi/attendance_registration_system/models/UserProjectModel.java
+++ attendance_registration_system/src/main/java/dev/tgsi/attendance_registration_system/models/UserProjectModel.java
@@ -1,28 +1,29 @@
 package dev.tgsi.attendance_registration_system.models;
 
-    import jakarta.persistence.*;
-    import lombok.Data;
-    import lombok.NoArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-    @Entity
-    @Table(name = "tbl_user_project")
-    @Data
-    @NoArgsConstructor
+@Entity
+@Table(name = "tbl_user_project")
+@Data
+@NoArgsConstructor
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+public class UserProjectModel {
 
-    public class UserProjectModel {
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+@Column(name = "upid")
+private Integer upid;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "upid")
-    private Integer upid;
+@ManyToOne
+@JoinColumn(name = "emp_id", referencedColumnName = "emp_id")
+private User user;
 
-    @ManyToOne
-    @JoinColumn(name = "emp_id", referencedColumnName = "emp_id")
-    private User user;
-
-    @ManyToOne
-    @JoinColumn(name = "proj_id", referencedColumnName = "proj_id")
-    private ProjectModel project;
+@ManyToOne
+@JoinColumn(name = "proj_id", referencedColumnName = "proj_id")
+private ProjectModel project;
     
 }
