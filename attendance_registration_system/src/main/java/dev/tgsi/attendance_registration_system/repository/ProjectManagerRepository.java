90eee98c28f0ef1d28c5ef9018897ffcff3b3d97
jude <quimiojude@gmail.com>
2025-02-20 08:13:43 +0800

    adding project manager repository to find the projects of the managers

---
+++ attendance_registration_system/src/main/java/dev/tgsi/attendance_registration_system/repository/ProjectManagerRepository.java
@@ -0,0 +1,16 @@
+package dev.tgsi.attendance_registration_system.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import dev.tgsi.attendance_registration_system.models.ProjectManagerModel;
+import dev.tgsi.attendance_registration_system.models.User;
+
+@Repository
+public interface ProjectManagerRepository extends JpaRepository<ProjectManagerModel, Integer>{
+    
+    List<ProjectManagerModel> findByUser(User user);
+
+}
