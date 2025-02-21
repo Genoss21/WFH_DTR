62e60f459a5f045435dc780dbb917dad95d6abc6
Tsukijay08 <delacruz.jojitjr.d.1458@gmail.com>
2025-02-21 13:24:59 +0800

    Error messages in login validation

--- attendance_registration_system/src/main/java/dev/tgsi/attendance_registration_system/configurations/SecurityConfig.java
+++ attendance_registration_system/src/main/java/dev/tgsi/attendance_registration_system/configurations/SecurityConfig.java
@@ -69,7 +69,7 @@ public class SecurityConfig {
                 .invalidateHttpSession(true)
                 .clearAuthentication(true)
                 .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                .logoutSuccessUrl("/login?logout")
+                .logoutSuccessUrl("/login")
                 .permitAll()
             );
 
