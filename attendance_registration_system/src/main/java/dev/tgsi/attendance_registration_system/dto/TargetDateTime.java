256cc2b224d406a14a34bd85c4b8b4e342fb6254
jude <quimiojude@gmail.com>
2025-02-25 12:21:44 +0800

    modified based on the reset time of DTR

--- attendance_registration_system/src/main/java/dev/tgsi/attendance_registration_system/dto/TargetDateTime.java
+++ attendance_registration_system/src/main/java/dev/tgsi/attendance_registration_system/dto/TargetDateTime.java
@@ -1,33 +1,43 @@
 package dev.tgsi.attendance_registration_system.dto;
 
 import java.time.LocalDate;
-//import java.time.LocalDateTime;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 
 public class TargetDateTime {
     
     public LocalDate getTargetDate(){
-        
-        LocalTime timeOut = LocalTime.now();
-        LocalDate date;
-        //LocalTime time = LocalTime.of(6, 0, 0);;
-       // LocalDateTime dateTime;
-        if(timeOut.isBefore(LocalTime.of(6, 0, 1))){
-            date = LocalDate.now();
-            //dateTime = date.atTime(time);
-        }
-        else{
-            date = LocalDate.now().plusDays(1);
-            ///dateTime = date.atTime(time);
-        }
-
+        LocalDate date = LocalDate.parse(LocalDate.now().toString());
         return date;
     }
 
-    public LocalDate getBeforeDate(){
+     public LocalDateTime getCurrendtDateTime(){
 
-        return getTargetDate().minusDays(1);
+        LocalDateTime now = LocalDateTime.parse(LocalDateTime.now().toString());
+        return now;
+
     }
 
+    public LocalDateTime getResetDate(){
+
+        LocalDate date;
+        LocalTime time = LocalTime.of(6, 00, 0);
+        LocalDateTime dateTime;
+        
+        date = LocalDate.now();
+        dateTime = date.atTime(time);
+        return dateTime;
+    }
+    public LocalDate getNextDay(){
+
+        LocalDate date = getTargetDate().plusDays(1);
+        return date;
+    }
+
+    public LocalDate getPreviousDay(){
+
+        return getTargetDate().minusDays(1);
+
+    }
 
 }
